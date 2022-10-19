b86d92223ca1d52fc26923ef685da28f2f1b6276
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-18 23:47:05 -0500

    fixed small bugs

--- src/ReturnMethod.java
+++ src/ReturnMethod.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ReturnMethod {
+// A program that uses the Vehicle Class
+
+class ReturnMethod{
+  public static void main(String[] args) {
+    Vehicle coupe = new Vehicle(); // create a Vehicle object named coupe
+    Vehicle minivan = new Vehicle(); //create a Vehicle object named minivan
+    int rangeC;
+    int rangeM;
+
+    //assign values to fields in coupe
+    coupe.passengers = 2;
+    coupe.fuelcap = 14;
+    coupe.mpg = 35;
+    rangeC = coupe.range();
+
+    //assign values to fields in minivan
+    minivan.passengers = 7;
+    minivan.fuelcap = 18;
+    minivan.mpg = 21;
+    rangeM = minivan.range();
+
+    // state the passenger amount and compute the range of a minivan assuming a full tank of gas
+    System.out.print("A coupe carries " + coupe.passengers + " passengers. The range is "
+        +  rangeC + ". ");
+
+    // state the passenger amount and compute the range of a minivan assuming a full tank of gas
+    System.out.print("A minivan carries " + minivan.passengers + " passengers. The range is "
+        + rangeM + ". ");
+  }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Vehicle {
+class Vehicle {
+  int passengers; // number of passengers
+  int fuelcap; // fuel capacity in gallons
+  int mpg; // fuel consumption in miles per gallon
+
+  int range() {
+     return fuelcap * mpg;
+  }
 }
